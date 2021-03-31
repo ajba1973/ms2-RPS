1d6b8ea1aa988f727231f86a3734e69fbdc51eec
ajba1973 <ajba1973@gmail.com>
2021-03-31 16:25:45 +0000

    Initiliaze main.js

---
+++ asset/js/main.js
@@ -0,0 +1,14 @@
+let userScore = 0;
+let pcScore = 0;
+// Cache DOM
+const userScore_span = document.getElementById('user-score');
+const pcScore_span = document.getElementById('pc-score');
+const scoreBoard_div = document.querySelector('scoreboard');
+const result_div = document.querySelector('.result p');
+const rock_div = document.getElementById('r');
+const paper_div = document.getElementById('p');
+const scissors_div = document.getElementById('s');
+const user_div = document.getElementById('player'); 
+const img_div = document.getElementById('img');
+const newGame_div = document.getElementById("newGame");
+
